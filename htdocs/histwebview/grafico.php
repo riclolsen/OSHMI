dc0432773d37506457dba3e7a2243a2d4d2b97eb
riclolsen <olsen.ricardo@gmail.com>
2023-04-13 15:12:37 -0300

    Removed unneeded files.

--- htdocs/histwebview/grafico.php
+++
@@ -1,8 +0,0 @@
-<?php
-// acerta variáveis globais para várias versões do PHP
-extract($_REQUEST, EXTR_PREFIX_ALL|EXTR_SKIP, 'p');
-
-$im = imagecreatefrompng ("$p_filename");
-imagepng ($im);
-?>
-
